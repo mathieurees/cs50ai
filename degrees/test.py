49ce1b7c1742b1bfea3ba21f1c115b4e5d04d1fd
mathieurees <mathieurees@gmail.com>
2026-02-25 14:47:37 +0000

    Trivial change; checking if non-commited changes affecting submit50

--- degrees/test.py
+++ degrees/test.py
@@ -43,3 +43,13 @@ class TestReturnsShortestLengthOnePath():
     def test_order_irrelevant(self):
         output = shortest_path("144", "1597")
         assert output == [("93779","1597")]
+
+class TestReturnsShortestLengthOnePath():
+
+    def test_correct_when_inputs_different(self):
+        output = shortest_path("1597", "144")
+        assert output == [("93779","144")] # source 1597 starred in movie 93779 with person 144
+
+    def test_order_irrelevant(self):
+        output = shortest_path("144", "1597")
+        assert output == [("93779","1597")]
