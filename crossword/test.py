d9a53096ab50265e8190bb34187376a3f897342d
mathieurees <mathieurees@gmail.com>
2026-07-28 14:28:11 +0100

    Refactor fixture to allow control over word file.

--- crossword/test.py
+++ crossword/test.py
@@ -15,32 +15,35 @@ def test_game_0():
 
     Accross is the accross word variable. Down is the down word variable.
 
-    The only words in the vocabulary are 'six' and 'seven'. (Yes.)
+    Default filepath to words test file is "./data/testwords0.txt".
     """
-    simple_game = CrosswordCreator(Crossword("./data/teststructure0.txt",
-                                        "./data/testwords0.txt"))
-    across = Variable(0, 1, "across", 3)
-    down = Variable(0, 1, "down", 5)
-    return simple_game, across, down
+    def _test_game_0(*test_words_file):
+        test_struct = "./data/teststructure0.txt"
+        test_words = test_words_file or "./data/testwords0.txt"
+        test_game = CrosswordCreator(Crossword(test_struct, test_words))
+        across = Variable(0, 1, "across", 3)
+        down = Variable(0, 1, "down", 5)
+        return test_game, across, down
+    return _test_game_0
 
 class TestEnforceNodeConsistency:
 
     def test_alters_domains(self, test_game_0):
-        simple_game = test_game_0[0]
+        simple_game = test_game_0()[0]
         initial_domains = deepcopy(simple_game.domains)
         simple_game.enforce_node_consistency()
         new_domains = simple_game.domains
         assert initial_domains != new_domains
 
     def test_removes_word_when_too_large(self, test_game_0):
-        simple_game = test_game_0[0]
-        across = test_game_0[1]
+        simple_game = test_game_0()[0]
+        across = test_game_0()[1]
         simple_game.enforce_node_consistency()
         assert "seven" not in simple_game.domains[across]
 
     def test_removes_word_when_too_small(self, test_game_0):
-        simple_game = test_game_0[0]
-        down = test_game_0[2]
+        simple_game = test_game_0()[0]
+        down = test_game_0()[2]
         simple_game.enforce_node_consistency()
         assert "six" not in simple_game.domains[down]
     
